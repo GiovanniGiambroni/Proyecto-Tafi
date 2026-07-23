1ad0bee2763e3251d172c21908d6832b3a6db9da
Giovanni Giambroni <giovannigiambroni@gmail.com>
2026-07-23 12:27:49 -0300

    Implemented A* Pathfinding Algorithm

--- Assets/Scripts/Pathfinding/NavigationManager.cs
+++ Assets/Scripts/Pathfinding/NavigationManager.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Interfaces;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -18,6 +19,7 @@ public class NavigationManager : MonoBehaviour
     void Start()
     {
         CreateNodes();
+        CreateConnetions();
     }
 
     void CreateNodes()
@@ -26,8 +28,8 @@ public class NavigationManager : MonoBehaviour
         {
             if (walkable.HasTile(pos))
             {
-                Vector3 worldPos = walkable.CellToWorld(pos) + walkable.tileAnchor;
-                Node node = Instantiate(NodePrefab).AddComponent<Node>();
+                Vector2 worldPos = walkable.CellToWorld(pos) + walkable.tileAnchor;
+                Node node = Instantiate(NodePrefab, transform).GetComponent<Node>();
                 node.transform.position = worldPos;
                 nodes.Add(node);
             }
@@ -36,11 +38,87 @@ public class NavigationManager : MonoBehaviour
 
     void CreateConnetions()
     {
-
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            for (int j = i + 1; j < nodes.Count; j++)
+            {
+                if (Vector2.Distance(nodes[i].transform.position, nodes[j].transform.position) < 1.5f)
+                {
+                    ConnectNodes(nodes[i], nodes[j]);
+                    ConnectNodes(nodes[j], nodes[i]);
+                }
+            }
+        }
     }
 
     void ConnectNodes(Node from, Node to)
     {
-        //if ()
+        if (from == to) return;
+        from.neighbors.Add(to);
+    }
+
+    public List<Node> GeneratePath(Node start, Node goal)
+    {
+        List<Node> openSet = new List<Node> { start };
+
+        foreach (Node node in nodes)
+        {
+            node.gScore = float.MaxValue;
+        }
+
+        // Inicializa los puntajes del nodo inicial
+        start.gScore = 0;
+        start.hScore = Vector2.Distance(start.transform.position, goal.transform.position);
+        openSet.Add(start);
+
+        while (openSet.Count > 0)
+        {
+            // Encuentra el nodo con el menor fScore en el openSet
+            int lowestFIndex = default;
+            for (int i = 0; i < openSet.Count; i++)
+            {
+                if (openSet[i].fScore < openSet[lowestFIndex].fScore)
+                {
+                    lowestFIndex = i;
+                }
+            }
+
+            // Obtiene el nodo actual y lo elimina del openSet
+            Node current = openSet[lowestFIndex];
+            openSet.RemoveAt(lowestFIndex);
+
+            // Si llega al nodo objetivo, reconstruye el camino y termina temprano
+            if (current == goal)
+            {
+                List<Node> path = new List<Node>();
+                while (current != null)
+                {
+                    path.Add(current);
+                    current = current.cameFrom;
+                }
+                path.Reverse();
+                return path;
+            }
+
+            // Busca en los vecinos y actualiza el openSet
+            foreach (Node neighbor in current.neighbors)
+            {
+                float heldGScore = current.gScore + Vector2.Distance(current.transform.position, neighbor.transform.position);
+
+                if (heldGScore < neighbor.gScore)
+                {
+                    neighbor.cameFrom = current;
+                    neighbor.gScore = heldGScore;
+                    neighbor.hScore = Vector2.Distance(neighbor.transform.position, goal.transform.position);
+
+                    if (!openSet.Contains(neighbor))
+                    {
+                        openSet.Add(neighbor);
+                    }
+                }
+            }
+        }
+
+        return null;
     }
 }
